309cf840bc21cd03c068a5c8cc8790c3d0879e3a
Andre <andre05117@gmail.com>
2025-08-17 20:28:40 -0500

    commissions

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-let gallerySections;
+let Sections;
 let content;
 let clase;
 
-function showSection(gallerySections,clase) {
+function showSection(Sections,clase) {
      content = document.getElementsByClassName(clase); 
 
     /* Hide all sections by default */
@@ -10,5 +10,5 @@ for (i=0; i < content.length; i++) {
     content[i].style.display = 'none';
 }
 
-    document.getElementById(gallerySections).style.display = 'flex';
+    document.getElementById(Sections).style.display = 'flex';
 }
