06749da25c4e8d7c8ef47c7386c4e55eea363087
Andre <andre05117@gmail.com>
2025-08-24 18:17:45 -0500

    tos

--- app.js
+++ app.js
@@ -12,3 +12,23 @@ for (i=0; i < content.length; i++) {
 
     document.getElementById(Sections).style.display = 'flex';
 }
+
+
+function popUpOpen(Sections) {
+    document.getElementById(Sections).style.display = 'block';
+    document.body.style.overflow = "hidden"; //Prevenir scroll de fondo
+}
+
+function popUpClose(Sections) {
+    document.getElementById(Sections).style.display = 'none';
+    document.body.style.overflow = ''; // Restaurar scroll
+}
+
+// Cerrar al hacer clic fuera del modal
+
+// Cerrar tosSection usando la tecla esc
+document.addEventListener('keydown', function(event) {
+    if (event.key === 'Escape') {
+        popUpClose('tosSection');
+    }
+})
